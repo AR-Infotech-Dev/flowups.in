a84c3afbd38fae1810de5f4122466722f6d6d906
AR-Infotech-DEV <arinfotechtdl@gmail.com>
2026-07-01 11:35:33 +0530

    Aniket : Code Pushed

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contact from '../components/ContactUs'
+
+function ContactUs() {
+  return (
+    <div className='py-5'>
+      <Contact />
+    </div>
+  )
+}
+
+export default ContactUs
